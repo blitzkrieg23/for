74fb7fc76ef929414e6f922a255a0f257ba5964d
xzqt23 <angelo.agualada@gmail.com>
2023-02-17 10:34:39 +0800

    Create for.py

---
+++ for.py
@@ -0,0 +1,18 @@
+fruits = ["🍎","🍓","🥭","🍒","🍏"]
+
+for x in fruits:
+    print(x)
+
+
+#############################
+
+# Loop on strings
+name = "John"
+for i in name:
+    print(i)
+
+#############################
+# Loop on numbers
+
+for z in range(1,11): # it will print 1-10, if you want to start on number 1 specify it.
+ print(z)        
