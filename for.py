d48852f4d1770062a86f6a2fdfe1df2831719540
xzqt23 <angelo.agualada@gmail.com>
2023-02-17 10:38:43 +0800

    Update for.py

--- for.py
+++ for.py
@@ -16,3 +16,14 @@ for i in name:
 
 for z in range(1,11): # it will print 1-10, if you want to start on number 1 specify it.
  print(z)        
+
+
+"""
+-This code will print 1,3,5,7,9....it starts from 1 up to 10 values but excluding 10
+and will increment by 2 
+
+
+for x in range(1, 10, 2):
+  print(x) 
+
+"""
